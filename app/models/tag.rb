339254e1fccf5815a96a39455b4adc6d07864ad9
missvalim <your.533masya7@mail.ru>
2018-12-21 17:11:52 +0500

    Improve the README file

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
 	has_many :taggings
 	has_many :posts, through: :taggings
-	def self.counts(taggings.count)
+	def self.taggings.count
 		self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")		
 	end
 
