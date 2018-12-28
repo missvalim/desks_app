cf95c1bc242a84a1ff7266cd9ba202a59f01cfc8
missvalim <your.533masya7@mail.ru>
2018-12-28 16:08:34 +0500

    some comment...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 	def bootstrap_class_for(name)
-		{
-			success: "alert-success",
+		{   success: "alert-success",
 			error: "alert-danger",
 			danger: "alert-danger",
 			alert: "alert-warning",
