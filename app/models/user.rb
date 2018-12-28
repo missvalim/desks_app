97ba5df75477e863afae2455a0c932a4bc5a455d
missvalim <your.533masya7@mail.ru>
2018-12-28 16:42:15 +0500

    some comment...

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
  has_many :posts
- belongs_to :userring
+
  mount_uploader :avatar, AvatarUploader
  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
